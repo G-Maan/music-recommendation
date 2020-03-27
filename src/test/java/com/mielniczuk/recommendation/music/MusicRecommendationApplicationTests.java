1c77fc7ecafc721d3c20c8b27559450a3febc71d
G-Maan <galaxiset@gmail.com>
2020-03-27 20:42:53 +0100

    initial

---
+++ src/test/java/com/mielniczuk/recommendation/music/MusicRecommendationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mielniczuk.recommendation.music;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicRecommendationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
