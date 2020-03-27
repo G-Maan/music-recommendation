59ae5d783232a9f642a75350638ea322b44d3771
G-Maan <galaxiset@gmail.com>
2020-03-27 21:12:29 +0100

    music info service project

---
+++ music-info-service/src/main/java/com/mielniczuk/recommendation/musicinfoservice/MusicInfoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mielniczuk.recommendation.musicinfoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicInfoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicInfoServiceApplication.class, args);
+	}
+
+}
---
+++ music-info-service/src/test/java/com/mielniczuk/recommendation/musicinfoservice/MusicInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mielniczuk.recommendation.musicinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
