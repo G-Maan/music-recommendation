ea51104e236e9bb5d5aacaf46e94cb0aeee306d7
G-Maan <galaxiset@gmail.com>
2020-03-31 13:01:02 +0200

    Added timeout to restTemplate and changed webClient url to discovery-service names

--- music-catalog-service/src/main/java/com/mielniczuk/recommendation/musiccatalogservice/MusicCatalogServiceApplication.java
+++ music-catalog-service/src/main/java/com/mielniczuk/recommendation/musiccatalogservice/MusicCatalogServiceApplication.java
@@ -2,7 +2,9 @@ package com.mielniczuk.recommendation.musiccatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -10,22 +12,27 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class MusicCatalogServiceApplication {
 
 	@Bean(name = "musicInfoWebClient")
+	@LoadBalanced // server discovery + load balance
 	public WebClient getMusicInfoWebClient() {
 		return WebClient.builder()
-				.baseUrl("http://localhost:8082/")
+				.baseUrl("http://music-info-service/")
 				.build();
 	}
 
 	@Bean(name = "ratingWebClient")
+	@LoadBalanced // server discovery + load balance
 	public WebClient getRatingWebClient() {
 		return WebClient.builder()
-				.baseUrl("http://localhost:8083/")
+				.baseUrl("http://ratings-data-service/")
 				.build();
 	}
 
 	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
+	@LoadBalanced // server discovery + load balance
+	public RestTemplate getRestTemplate() {
+		HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+		clientHttpRequestFactory.setConnectTimeout(3000); // timeout of 3s
+		return new RestTemplate(clientHttpRequestFactory);
 	}
 
 	public static void main(String[] args) {
