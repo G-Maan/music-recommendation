b3f52936f10f59574696e9c6c11397d3adc7186b
G-Maan <galaxiset@gmail.com>
2020-03-31 13:07:45 +0200

    Deleted redundant interfaces and changed naming convention for services

--- music-info-service/src/main/java/com/mielniczuk/recommendation/musicinfoservice/services/MusicService.java
+++ music-info-service/src/main/java/com/mielniczuk/recommendation/musicinfoservice/services/MusicService.java
@@ -1,9 +1,28 @@
 package com.mielniczuk.recommendation.musicinfoservice.services;
 
+import com.mielniczuk.recommendation.musicinfoservice.models.Music;
 import com.mielniczuk.recommendation.musicinfoservice.models.dtos.MusicDTO;
+import com.mielniczuk.recommendation.musicinfoservice.repositories.MusicRepository;
+import org.springframework.stereotype.Service;
 
-public interface MusicService {
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
-    public MusicDTO getMusic(int musicId);
+@Service
+public class MusicService {
 
+    private MusicRepository musicRepository;
+
+    public MusicService(MusicRepository musicRepository) {
+        this.musicRepository = musicRepository;
+    }
+
+    public MusicDTO getMusic(int musicId) {
+        Optional<Music> music = this.musicRepository.findById(musicId);
+        if (music.isPresent()) {
+           return new MusicDTO(music.get());
+        } else {
+            throw new NoSuchElementException();
+        }
+    }
 }
--- ratings-data-service/src/main/java/com/mielniczuk/recommendation/ratingsdataservice/services/RatingService.java
+++ ratings-data-service/src/main/java/com/mielniczuk/recommendation/ratingsdataservice/services/RatingService.java
@@ -1,11 +1,41 @@
 package com.mielniczuk.recommendation.ratingsdataservice.services;
 
+import com.mielniczuk.recommendation.ratingsdataservice.models.Rating;
+import com.mielniczuk.recommendation.ratingsdataservice.models.UserRating;
 import com.mielniczuk.recommendation.ratingsdataservice.models.dtos.RatingDTO;
 import com.mielniczuk.recommendation.ratingsdataservice.models.dtos.RatingListDTO;
+import com.mielniczuk.recommendation.ratingsdataservice.repositories.RatingRepository;
+import com.mielniczuk.recommendation.ratingsdataservice.repositories.UserRatingRepository;
+import org.springframework.stereotype.Service;
 
-public interface RatingService {
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
-    public RatingDTO getRating(int ratingId);
-    public RatingListDTO getUserRatings(int userId);
+@Service
+public class RatingService {
 
+    private final RatingRepository ratingRepository;
+
+    private final UserRatingRepository userRatingRepository;
+
+    public RatingService(RatingRepository ratingRepository,
+                         UserRatingRepository userRatingRepository) {
+        this.ratingRepository = ratingRepository;
+        this.userRatingRepository = userRatingRepository;
+    }
+
+    public RatingDTO getRating(int ratingId) {
+        Optional<Rating> rating = ratingRepository.findById(ratingId);
+        if (rating.isPresent()) {
+            return new RatingDTO(rating.get());
+        } else {
+            throw new NoSuchElementException();
+        }
+    }
+
+    public RatingListDTO getUserRatings(int userId) {
+        Optional<UserRating> userRating = userRatingRepository.findById(userId);
+        return userRating.map(usr -> new RatingListDTO(usr.getRatings().stream().map(RatingDTO::new).collect(Collectors.toList()))).orElseThrow();
+    }
 }
