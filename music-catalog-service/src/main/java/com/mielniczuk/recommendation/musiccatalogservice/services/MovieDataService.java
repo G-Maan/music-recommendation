2568afaebd072a598e43c3bc6e80230213a9f3f2
G-Maan <galaxiset@gmail.com>
2020-04-02 16:32:27 +0200

    Added Hystrix dashboard and upgraded spring-cloud.version to Hoxton

--- music-catalog-service/src/main/java/com/mielniczuk/recommendation/musiccatalogservice/services/MovieDataService.java
+++ music-catalog-service/src/main/java/com/mielniczuk/recommendation/musiccatalogservice/services/MovieDataService.java
@@ -4,6 +4,7 @@ import com.mielniczuk.recommendation.musiccatalogservice.models.dto.MovieDataDTO
 import com.mielniczuk.recommendation.musiccatalogservice.models.dto.MovieDataListDTO;
 import com.mielniczuk.recommendation.musiccatalogservice.models.dto.MovieRatingIDListDTO;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -17,7 +18,13 @@ public class MovieDataService {
 
     private final RestTemplate restTemplate;
 
-    @HystrixCommand(fallbackMethod = "getMovieDataListFallback")
+    @HystrixCommand(fallbackMethod = "getMovieDataListFallback",
+            commandProperties = {
+                    @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000"),
+                    @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "5"),
+                    @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "50"),
+                    @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "5000")
+            })
     public MovieDataListDTO getMovieDataList(MovieRatingIDListDTO movieRatingIDListDTO) {
         return restTemplate.postForObject("http://music-info-service/movies/list/", movieRatingIDListDTO, MovieDataListDTO.class);
     }
--- music-catalog-service/src/main/java/com/mielniczuk/recommendation/musiccatalogservice/services/MovieRatingService.java
+++ music-catalog-service/src/main/java/com/mielniczuk/recommendation/musiccatalogservice/services/MovieRatingService.java
@@ -2,6 +2,7 @@ package com.mielniczuk.recommendation.musiccatalogservice.services;
 
 import com.mielniczuk.recommendation.musiccatalogservice.models.dto.MovieRatingListDTO;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -14,7 +15,13 @@ public class MovieRatingService {
 
     private final RestTemplate restTemplate;
 
-    @HystrixCommand(fallbackMethod = "getMovieRatingListFallback")
+    @HystrixCommand(fallbackMethod = "getMovieRatingListFallback",
+            commandProperties = {
+                    @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000"),
+                    @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "5"),
+                    @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "50"),
+                    @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "5000")
+            })
     public MovieRatingListDTO getMovieRatingList(Long userId) {
         return restTemplate.getForObject("http://ratings-data-service/ratings/users/" + userId, MovieRatingListDTO.class);
     }
